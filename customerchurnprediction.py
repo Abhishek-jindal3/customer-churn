bada1e6c6de41a073d98520e0418ebce4255facc
Abhishek-jindal3 <prajapatiraviranjan3@gmail.com>
2026-05-05 23:47:51 +0530

    Update customerchurnprediction.py

--- customerchurnprediction.py
+++ customerchurnprediction.py
@@ -1,25 +1,32 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow import keras
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report,confusion_matrix
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 df=pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
 df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
 df=df[df['TotalCharges'].notnull()]
 df=df.drop('customerID',axis=1)
 
-from sklearn.preprocessing import MinMaxScaler
+
 scaler = MinMaxScaler()
 df[['TotalCharges','MonthlyCharges','tenure']] = scaler.fit_transform(df[['TotalCharges','MonthlyCharges','tenure']])
 df.replace({'No phone service': 'No', 'No internet service': 'No'},inplace=True)
 df.replace({'Male': 0, 'Female': 1, 'Yes': 1, 'No': 0}, inplace=True)
 df = pd.get_dummies(df, columns=['InternetService','Contract','PaymentMethod'], drop_first=True)
-from sklearn.model_selection import train_test_split
+
 X = df.drop('Churn', axis=1)
 print(X.shape)
 y = df['Churn']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print(X_train.shape)
-import tensorflow as tf
-from tensorflow import keras
+
 model=keras.Sequential([
     keras.layers.Input(shape=(23,)),
     keras.layers.Dense(16, input_shape=(23,), activation='relu'),
@@ -29,9 +36,7 @@ model=keras.Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 model.fit(X_train, y_train, epochs=20)
 y_pred = (model.predict(X_test) > 0.5).astype(int)
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report,confusion_matrix
+
 print(classification_report(y_test, y_pred))
 sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted')
